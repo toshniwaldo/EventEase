3b501b1c9e57660a147447466abe722895c170e7
Dhruv Toshniwal <134626880+toshniwaldo@users.noreply.github.com>
2025-09-18 11:26:55 +0530

    Update Login.jsx

--- GeofenceAutomaticTicketValidation - Copy/frontend/src/pages/Login.jsx
+++ GeofenceAutomaticTicketValidation - Copy/frontend/src/pages/Login.jsx
@@ -26,7 +26,7 @@ export default function Login() {
     setIsLoading(true)
 
     try {
-      const response = await fetch("http://localhost:5000/auth/login", {
+      const response = await fetch("https://backend-assh.onrender.com/auth/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
@@ -225,3 +225,4 @@ export default function Login() {
     </div>
   )
 }
+
